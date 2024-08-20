31fa157dbde2c9bd2997aef4623bdcf9ed7aa6a4
Dangkeys <66010360@kmitl.ac.th>
2024-08-20 18:26:54 +0700

    feat: Remove unused namespaces and dependencies

--- Assets/Scripts/GamePlay/Door.cs
+++ Assets/Scripts/GamePlay/Door.cs
@@ -1,8 +1,5 @@
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using System.Collections;
-using System.Collections.Generic;
-using Microsoft.Unity.VisualStudio.Editor;
 
 public class Door : MonoBehaviour
 {
