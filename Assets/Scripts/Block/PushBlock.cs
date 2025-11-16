e15716e6e3ed18b895eb85639b748e0dc74e71a5
Taj Dangkomen <66010360@kmitl.ac.th>
2025-11-16 11:46:24 +0700

    feat: update PushBlock to handle player scaling and mass adjustment

--- Assets/Scripts/Block/PushBlock.cs
+++ Assets/Scripts/Block/PushBlock.cs
@@ -1,42 +1,61 @@
+// ...existing code...
+using System;
+using System.Linq;
 using UnityEngine;
 
 public class PushBlock : MonoBehaviour
 {
     private Rigidbody2D rb;
     private BoxCollider2D boxCollider;
+    private Player player1;
+    [SerializeField] private LayerMask playerLayerMask;
+
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         boxCollider = GetComponent<BoxCollider2D>();
+        player1 = FindObjectsByType<Player>(FindObjectsSortMode.None)
+            .FirstOrDefault(p => p.IsPlayerOne);
+        if (player1 != null)
+            player1.OnChangeScale += ShouldChangeMass;
+    }
+
+    private void ShouldChangeMass(bool obj = true)
+    {
+        if (boxCollider == null || rb == null) return;
+
+        Vector2 center = transform.TransformPoint(boxCollider.offset);
+        Vector2 size = Vector2.Scale(boxCollider.size, new Vector2(transform.lossyScale.x, transform.lossyScale.y));
+        float angle = transform.eulerAngles.z;
+
+        Collider2D[] hitColliders = Physics2D.OverlapBoxAll(center, size, angle, playerLayerMask.value);
+        foreach (var hitCollider in hitColliders)
+        {
+            if (hitCollider.TryGetComponent(out Player player))
+            {
+                if (player == player1 && player.IsScaled)
+                {
+                    rb.mass = 1f;
+                    return;
+                }
+            }
+        }
+        rb.mass = 1000f;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.collider is CapsuleCollider2D capsuleCollider2D)
-        {
-            if (collision.gameObject.TryGetComponent(out Player player))
-            {
-                if (capsuleCollider2D.size.y * collision.transform.localScale.y >= boxCollider.size.y * transform.localScale.y)
-                {
-                    if(player.IsPlayerOne && player.CheckIfOnMacroBlock()) return;
-                    rb.mass = 1f;
-                }
-                else
-                {
-                    rb.mass = 1000f;
-                }
-            }
-        }
+        ShouldChangeMass();
     }
 
     private void OnCollisionExit2D(Collision2D collision)
     {
-        if (collision.collider is CapsuleCollider2D capsuleCollider2D)
-        {
-            if (collision.gameObject.TryGetComponent(out Player player))
-            {
-                rb.mass = 1000f;
-            }
-        }
+        ShouldChangeMass();
+    }
+
+    private void OnDestroy()
+    {
+        if (player1 != null)
+            player1.OnChangeScale -= ShouldChangeMass;
     }
 }
